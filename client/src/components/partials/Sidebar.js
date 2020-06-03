bf9fc2c4a9baadf37e9f494febfe1a276fda5ef3
hkolli <hkolli@ftdi.com>
2020-06-03 17:47:33 +0530

    added all menus

--- client/src/components/partials/Sidebar.js
+++ client/src/components/partials/Sidebar.js
@@ -53,6 +53,54 @@ const Sidebar = ( props ) => {
                                 <Link to="/eb-list" className="nav-link" >Add EB Entry</Link>
                                 <Link to="/eb-due-list" className="nav-link">EB Due List</Link>
                                 <Link to="/consolidated-eb-list" className="nav-link">Consolidated EB List</Link>
+                                <Link to="/" className="nav-link">Schedule An EB</Link>
+                                <Link to="/" className="nav-link">Scheduled EB List</Link>
+                            </nav>
+                            <a className="nav-link collapsed" href="#fileManager" data-toggle="collapse" onClick={ expandMenu }>
+                                File Manager 
+                                <i 
+                                    className={classnames("fas", {
+                                        'fa-chevron-down' : menuName === "#fileManager",
+                                        'fa-chevron-right' : menuName !== "#fileManager"
+                                    })}
+                                ></i>
+                            </a>
+                            <nav className={
+                                classnames("nav nav-pills flex-column", {
+                                    'collapse' : menuName !== "#fileManager"
+                                })} 
+                                id="fileManager">
+                                <Link to="/eb-list" className="nav-link" >Send File To IFA</Link>
+                                <Link to="/eb-due-list" className="nav-link">Receive File From IFA</Link>
+                                <Link to="/consolidated-eb-list" className="nav-link">Resend File To IFA</Link>
+                                <Link to="/" className="nav-link">Pending File List At IFA</Link>
+                                <Link to="/" className="nav-link">Concurred File List</Link>
+                            </nav>
+                            <a className="nav-link collapsed" href="#projectManager" data-toggle="collapse" onClick={ expandMenu }>
+                                Project Manager 
+                                <i 
+                                    className={classnames("fas", {
+                                        'fa-chevron-down' : menuName === "#projectManager",
+                                        'fa-chevron-right' : menuName !== "#projectManager"
+                                    })}
+                                ></i>
+                            </a>
+                            <nav className={
+                                classnames("nav nav-pills flex-column", {
+                                    'collapse' : menuName !== "#projectManager"
+                                })} 
+                                id="projectManager">
+                                <Link to="/eb-list" className="nav-link" >Add New Sanctioned Project</Link>
+                                <Link to="/eb-due-list" className="nav-link">View Ongoing Project List</Link>
+                                <Link to="/consolidated-eb-list" className="nav-link">Add Amendment</Link>
+                                <Link to="/" className="nav-link">View Project Details With Amendments</Link>
+                                <Link to="/" className="nav-link">Add New Project Proposal</Link>
+                                <Link to="/" className="nav-link">Change Status Of New Project Proposal</Link>
+                                <Link to="/" className="nav-link">CCM Cleared But Due For DMC </Link>
+                                <Link to="/" className="nav-link">Projects To Be Represented In CCM </Link>
+                                <Link to="/" className="nav-link">DMC Cleared But Due For Sanction </Link>
+                                <Link to="/" className="nav-link">Projects To Be Represented In DMC</Link>
+                                <Link to="/" className="nav-link">List Of New Projects In Pipeline </Link>
                             </nav>
                             <a href="#" className="nav-link" onClick={onLogoutClick}>Logout <FontAwesomeIcon icon={faSignOutAlt} /></a>
                             
